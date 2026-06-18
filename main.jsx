3b6b2e218680296d0f8f50ec3f9f56e56854915b
Kunal <93034762+knlgwr@users.noreply.github.com>
2026-06-18 08:12:09 +0530

    Initialize main.jsx with React setup

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app.jsx' // Make sure your app file is named exactly app.jsx (all lowercase)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
